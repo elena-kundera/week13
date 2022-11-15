ede0cafe7e3757fe7d06abcb723c601ae4ee34d6
Елена Мусина <elena@MacBook-Air-Elena.local>
2022-11-15 22:54:35 +0300

    My first js

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function SayHi() {
+let name = prompt('Как тебя зовут?', '');
+alert(`${name}, очень приятно познакомиться!`);
+}
+
+
+
+function AskAge(age) {
+    age = prompt('А сколько тебе лет?', '');
+    alert('Ого! ' + age +'! Ты такой старый!');
+}
+
+
+
